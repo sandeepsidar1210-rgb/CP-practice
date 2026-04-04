7cd22f17c378887211aaf1d213e1a8551b503f7a
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-04 09:17:16 +0530

    oddoneout

---
+++ oddoneout.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (){
+    int t;
+    scanf("%d",&t);
+
+
+    for (int i=0; i<t; i++){
+     int a, b, c;
+    scanf("%d %d %d",&a ,&b ,&c);
+
+    if(a==b){
+        printf("%d\n",c);
+    }
+    else if (b==c)
+    {
+      printf("%d\n", a);
+    }
+    else{
+        printf("%d\n",b);
+    }
+    }
+   
+return 0;
+}
