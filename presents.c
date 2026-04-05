9b620b8f9101e8de2c2665df1933f30a6db61355
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-06 02:54:35 +0530

    Update presents solution

--- presents.c
+++ presents.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main(){
     int n;
-    int result[n];
-
     scanf("%d",&n);
+
     int arr[n];
-
-    for (int i =0;i<n;i++){
+    for(int i=0; i<n;i++){
         scanf("%d",&arr[i]);
-    }
-    
-    for(int i=0; i<n; i++){
-        result[arr[i]-1] = arr[i];
 
     }
 
-    for(int i=0; i<n ; i++){
-        printf("%d",result[i]);
+    for(int i=1 ; i<=n ; i++){
+        for(int j=0 ; j<n; j++){
+            if (arr[j] == i ){
+                printf("%d ", j + 1);
+                break;
+            }
+            
+            }
+        
+        }
+        return 0;
     }
 
-
-    return 0;
-}
