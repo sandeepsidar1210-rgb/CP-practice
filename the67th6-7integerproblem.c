d00d5cc3199bdb23260d5fb9548198f6a5fb8f17
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-12 17:09:48 +0530

    the 67th 6-7 integer problem

--- the67th6-7integerproblem.c
+++ the67th6-7integerproblem.c
@@ -2,8 +2,35 @@
 int main(){
     int t;
     scanf("%d",&t);
-    int arr[7];
+  
     for (int i=0; i<t; i++){
+        int arr[7];
+
+        for(int i=0; i<7; i++){
+            scanf("%d", &arr[i]);
+        }
+
+        int max_int = arr[0];
+        int index=0;
+
+        for(int i=1; i<7; i++){
+            if(arr[i]>max_int){
+                max_int = arr[i];
+                index = i;
+                
+            }
+        }
+        int sum=0;
+        for(int i=0; i<7; i++){
+            if(i == index){
+                sum = sum + arr[i];
+            }
+            else{
+               sum = sum + (arr[i] * -1);
+            }
+        }
         
+       printf("%d\n",sum);
     }
+    return 0;
 }
