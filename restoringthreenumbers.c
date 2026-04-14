4f6010d5c88c8ef1412292cbda8953d227bf71dc
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-15 02:27:47 +0530

    rectified restoringthreenumbers

--- restoringthreenumbers.c
+++ restoringthreenumbers.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
-int main(){
-    int arr[4];
-    int a, b, c;
 
-    scanf("%d %d %d %d",&arr[0] , arr[1], arr[2], arr[3]);
-    int max =arr[0];
-    for(int i=1; i<4; i++){
-        if(arr[i]>max){
-            max = arr[i];
+int main(){
+    int x[4];
+
+    scanf("%d %d %d %d", &x[0], &x[1], &x[2], &x[3]);
+
+
+    int max = x[0];
+    for(int i = 1; i < 4; i++){
+        if(x[i] > max){
+            max = x[i];
         }
-        
     }
 
+    int a, b, c;
+    int j = 0;
 
+    for(int i = 0; i < 4; i++){
+        if(x[i] != max){
+            if(j == 0) a = max - x[i];
+            else if(j == 1) b = max - x[i];
+            else c = max - x[i];
+            j++;
+        }
+    }
+
+    printf("%d %d %d\n", a, b, c);
+
+    return 0;
 }
