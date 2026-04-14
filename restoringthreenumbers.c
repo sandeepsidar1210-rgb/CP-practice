3ad4866113606c827187f3364afa5081e36703ee
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-15 01:46:59 +0530

    restoringthreenumbers

---
+++ restoringthreenumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int arr[4];
+    int a, b, c;
+
+    scanf("%d %d %d %d",&arr[0] , arr[1], arr[2], arr[3]);
+    int max =arr[0];
+    for(int i=1; i<4; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+        
+    }
+
+
+}
