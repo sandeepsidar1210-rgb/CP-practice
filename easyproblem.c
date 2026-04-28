42acf4f1e8d372123305836930b069fc3a2f7e50
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-29 01:29:36 +0530

    easyproblem

---
+++ easyproblem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+
+    for (int i=0 ; i<t ; i++){
+        int n;
+        scanf("%d",&n);
+        int count = n-1;
+       
+
+        printf("%d\n",count);
+       
+    }
+return 0;
+}
